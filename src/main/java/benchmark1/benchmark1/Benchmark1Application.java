35bc3fc8140985ea00d6a573fac6754e1b7af3e3
Jeremy Olson <jeremy.olson@firstdata.com>
2020-07-26 21:12:03 -0500

    test

---
+++ src/main/java/benchmark1/benchmark1/Benchmark1Application.java
@@ -0,0 +1,13 @@
+package benchmark1.benchmark1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Benchmark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Benchmark1Application.class, args);
+	}
+
+}
--- src/test/java/benchmark1/benchmark1/Benchmark1ApplicationTests.java
+++ src/test/java/benchmark1/benchmark1/Benchmark1ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.benchmark.benchmark;
+package benchmark1.benchmark1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BenchmarkApplicationTests {
+class Benchmark1ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
