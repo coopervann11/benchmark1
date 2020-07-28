66d01739d0c01ca69abb18d4749551dea99a8de0
Jeremy Olson <jeremy.olson@firstdata.com>
2020-07-27 21:11:27 -0500

    4

--- src/main/java/benchmark1/benchmark1/WebController.java
+++ src/main/java/benchmark1/benchmark1/WebController.java
@@ -14,46 +14,57 @@ import java.io.FileReader;
 import java.io.Reader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class WebController {
 
     @GetMapping("/benchmark")
-    public String mainWithParam(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) throws Exception {
+    public String mainWithParam(@RequestParam(name = "name", required = false, defaultValue = "") final String name, final Model model) throws Exception {
 
-        File file = new ClassPathResource("/static/MOCK_DATA.csv").getFile();
-        Reader reader = new BufferedReader(new FileReader(file));
-        CsvToBean<User> csvToBean = new CsvToBeanBuilder(reader)
+        final File file = new ClassPathResource("/static/MOCK_DATA.csv").getFile();
+        final Reader reader = new BufferedReader(new FileReader(file));
+        final CsvToBean<User> csvToBean = new CsvToBeanBuilder(reader)
                 .withType(User.class)
                 .withIgnoreLeadingWhiteSpace(true)
                 .build();
-        List<User> users = csvToBean.parse();
-        StringBuilder foo1 = new StringBuilder();
-        StringBuffer foo2 = new StringBuffer();
+        final List<User> users = csvToBean.parse();
+        final StringBuilder foo1 = new StringBuilder();
+        final StringBuffer foo2 = new StringBuffer();
         // 1
-        long start1 = System.currentTimeMillis();
+        final long start1 = System.currentTimeMillis();
+        final List<User> results1 = new ArrayList<User>();
         for (User u : users) {
-            foo1.append(u.getFirstName() + "<br>");
+            if(u.getLastName().startsWith(name.toUpperCase())) {
+                results1.add(u);
+            }
         }
-        long duration1 = System.currentTimeMillis() - start1;
-        DateFormat sdf1 = new SimpleDateFormat("ss.SSS");
-        String stringDate1 = sdf1.format(duration1);
+        final long duration1 = System.currentTimeMillis() - start1;
+        final DateFormat sdf1 = new SimpleDateFormat("ss.SSS");
+        final String stringDate1 = sdf1.format(duration1);
         // 2
-        long start2 = System.currentTimeMillis();
-        users.forEach(u -> foo2.append(u + "<br>"));
-        long duration2 = System.currentTimeMillis() - start2;
-        DateFormat sdf2 = new SimpleDateFormat("ss.SSS");
-        String stringDate2 = sdf2.format(duration2);
+        final long start2 = System.currentTimeMillis();
+        final List<User> results2 = users.stream()
+                .filter(person -> person.getLastName().startsWith(name.toUpperCase()))
+                .sorted(Comparator.comparing(User::getLastName))
+                .collect(Collectors.toList());
+        final long duration2 = System.currentTimeMillis() - start2;
+        final DateFormat sdf2 = new SimpleDateFormat("ss.SSS");
+        final String stringDate2 = sdf2.format(duration2);
         // HTML Template
         model.addAttribute("diffInSeconds1", stringDate1);
         model.addAttribute("diffInSeconds2", stringDate2);
+        model.addAttribute("results1", results1);
+        model.addAttribute("results2", results2);
         model.addAttribute("name", name);
-        return "benchmark"; //view
+        return "benchmark";
     }
 
     @GetMapping("/")
     public String mainWithParam() {
-        return "index"; //view
+        return "index";
     }
 }
